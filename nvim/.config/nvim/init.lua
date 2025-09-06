547c1709961b7e78022307bab27f66371ae790db
Skewed <philipp.humburg.dev@gmail.com>
2025-09-06 21:18:38 +0200

    rewrite to single file approach to get a working config first before separating it, using mason

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,5 +1,20 @@
-require('skewed.core')
-require('skewed.lazy')
-require('skewed.core.init')
-require('skewed.colorscheme')
+-- Set leader key
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+
+require('keymaps')
+
+-- Bootstrap Lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", lazypath
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup("plugins")
 
